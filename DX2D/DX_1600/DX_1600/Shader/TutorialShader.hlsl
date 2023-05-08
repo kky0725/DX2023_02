3222f34c1a8ca32904c8bdd0ecf50a71e29cfe3d
4awol <129160510+kky0725@users.noreply.github.com>
2023-05-08 17:55:16 +0900

    DX2D
    1

---
+++ DX2D/DX_1600/DX_1600/Shader/TutorialShader.hlsl
@@ -0,0 +1,10 @@
+float4 VS( float4 pos : POSITION ) : SV_POSITION // Symentic Name... SV -> SysytemValue
+{
+
+	return pos;
+}
+
+float4 PS() : SV_TARGET
+{
+	return float4(1.0f, 1.0f, 0.0f, 1.0f);
+}
